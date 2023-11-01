b41c71bf6b3b7e24dfc33613152185ede0dbb614
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-01 22:23:06 +0530

    cleaning the project

--- src/main/java/com/kannada/KannadaApplication.java
+++ src/main/java/com/kannada/KannadaApplication.java
@@ -10,7 +10,7 @@ public class KannadaApplication
 	public static void main(String[] args)
 	{
 		SpringApplication.run(KannadaApplication.class, args);
-		System.out.println("\n Kannada Tayi");
+		System.out.println("\n******Kannada Tayi******\n");
 	}
 
 }
