792098306e79da591a585cdb96f18a0408f813a1
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-01 22:01:47 +0530

    Kannada Rajyostava

---
+++ src/main/java/com/kannada/KannadaApplication.java
@@ -0,0 +1,13 @@
+package com.kannada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KannadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KannadaApplication.class, args);
+	}
+
+}
